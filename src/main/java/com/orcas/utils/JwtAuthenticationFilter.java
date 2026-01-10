782ddb86bacf495333527278438c77b87ca246ce
ParthiKrish6 <parthiban30007@gamil.com>
2026-01-10 16:39:42 +0530

    Privacy

--- src/main/java/com/orcas/utils/JwtAuthenticationFilter.java
+++ src/main/java/com/orcas/utils/JwtAuthenticationFilter.java
@@ -17,46 +17,49 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final JwtUtil jwtUtil; 
-    private final List<String> skipServices = Arrays.asList(MappingConstants.URL_API_V1+MappingConstants.URL_LOGIN, MappingConstants.URL_API_V1+MappingConstants.HEALTH_CHECK);
+	private final JwtUtil jwtUtil;
+	private final List<String> skipServices = Arrays.asList(MappingConstants.URL_API_V1 + MappingConstants.URL_LOGIN,
+			MappingConstants.URL_API_V1 + MappingConstants.HEALTH_CHECK,
+			"/privacy.html");
 
-    public JwtAuthenticationFilter(JwtUtil jwtUtil) {
-        this.jwtUtil = jwtUtil;
-    }
+	public JwtAuthenticationFilter(JwtUtil jwtUtil) {
+		this.jwtUtil = jwtUtil;
+	}
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-    	
-    	System.out.println(request.getRequestURI());
-    	if(skipServices.contains(request.getRequestURI()) || "OPTIONS".equals(request.getMethod())) {
-    		System.out.println("JWT is skipped");
-    		filterChain.doFilter(request, response); 
-    		return;
-    	} else {
-    		String authorizationHeader = request.getHeader("Authorization");
-            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-                String jwt = authorizationHeader.substring(7);
-                if(jwtUtil.isExpired(jwt)) {
-                	System.out.println("JWT token Expired");
-                	response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                    return;
-                } else {
-                	System.out.println("JWT token Validated");
-                	if(Arrays.asList("POST", "PATCH", "DELETE").contains(request.getMethod()) && !jwtUtil.extractRole(jwt).equals("A")) {
-                		System.out.println("JWT Role Not Present");
-                    	response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                        return;
-                	} else {
-                		filterChain.doFilter(request, response);
-                    	return;
-                	}
-                }
-            } else {
-            	System.out.println("JWT token Not Present");
-            	response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                return;
-            }
-    	}
-    }
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+
+		System.out.println(request.getRequestURI());
+		if (skipServices.contains(request.getRequestURI()) || "OPTIONS".equals(request.getMethod())) {
+			System.out.println("JWT is skipped");
+			filterChain.doFilter(request, response);
+			return;
+		} else {
+			String authorizationHeader = request.getHeader("Authorization");
+			if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+				String jwt = authorizationHeader.substring(7);
+				if (jwtUtil.isExpired(jwt)) {
+					System.out.println("JWT token Expired");
+					response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+					return;
+				} else {
+					System.out.println("JWT token Validated");
+					if (Arrays.asList("POST", "PATCH", "DELETE").contains(request.getMethod())
+							&& !jwtUtil.extractRole(jwt).equals("A")) {
+						System.out.println("JWT Role Not Present");
+						response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+						return;
+					} else {
+						filterChain.doFilter(request, response);
+						return;
+					}
+				}
+			} else {
+				System.out.println("JWT token Not Present");
+				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+				return;
+			}
+		}
+	}
 }
